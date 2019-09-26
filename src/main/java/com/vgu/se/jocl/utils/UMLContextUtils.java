51f8c00fff1410dd2185856b521045e38c5d56b9
Hoang Nguyen <ngpbhoang1406@gmail.com>
2019-09-26 18:11:36 +0700

    javadoc for UMLContextUtils

--- src/main/java/com/vgu/se/jocl/utils/UMLContextUtils.java
+++ src/main/java/com/vgu/se/jocl/utils/UMLContextUtils.java
@@ -16,24 +16,40 @@ limitations under the License.
 @author: ngpbh
 ***************************************************************************/
 
-
 package com.vgu.se.jocl.utils;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+/**
+ * The Class UMLContextUtils contains various methods for querying over UML context
+ */
 public class UMLContextUtils {
+    
+    /**
+     * Validate UML Context.
+     *
+     * @param context the UML context
+     * @return true, if valid
+     */
     public static boolean validateContext(JSONArray context) {
         // TODO: Implement.
         return false;
     }
-    
+
+    /**
+     * Checks if {@code className} is an actual class in this context.
+     *
+     * @param context the UML context
+     * @param className the class name
+     * @return true, if this class is valid
+     */
     public static boolean isClass(JSONArray context, String className) {
-        if(context != null && className != null) {
-            for(Object entity : context) {
-                if(entity instanceof JSONObject) {
+        if (context != null && className != null) {
+            for (Object entity : context) {
+                if (entity instanceof JSONObject) {
                     JSONObject obj = (JSONObject) entity;
-                    if(className.equals(obj.get("class"))) {
+                    if (className.equals(obj.get("class"))) {
                         return true;
                     }
                 }
@@ -41,13 +57,20 @@ public class UMLContextUtils {
         }
         return false;
     }
-    
+
+    /**
+     * Checks if {@code associationName} is an actual association in this context.
+     *
+     * @param context the UML context
+     * @param associationName the association name
+     * @return true, if the association is valid
+     */
     public static boolean isAssociationClass(JSONArray context, String associationName) {
-        if(context != null && associationName != null) {
-            for(Object entity : context) {
-                if(entity instanceof JSONObject) {
+        if (context != null && associationName != null) {
+            for (Object entity : context) {
+                if (entity instanceof JSONObject) {
                     JSONObject jObject = (JSONObject) entity;
-                    if(associationName.equals(jObject.get("association"))) {
+                    if (associationName.equals(jObject.get("association"))) {
                         return true;
                     }
                 }
@@ -55,20 +78,30 @@ public class UMLContextUtils {
         }
         return false;
     }
-    
+
+    /**
+     * Checks if {@code propertyName} is an actual property of 
+     * {@code className} in this UML context.
+     *
+     * @param context the UML context
+     * @param className the class name
+     * @param propertyName the property name
+     * @return true, if is property belongs to the class 
+     * and the class also belongs to the UML context
+     */
     public static boolean isPropertyOfClass(JSONArray context, String className, String propertyName) {
-        if(context != null && className != null && propertyName != null) {
-            for(Object entity : context) {
-                if(entity instanceof JSONObject) {
+        if (context != null && className != null && propertyName != null) {
+            for (Object entity : context) {
+                if (entity instanceof JSONObject) {
                     JSONObject jObject = (JSONObject) entity;
-                    if(className.equals(jObject.get("class"))) {
+                    if (className.equals(jObject.get("class"))) {
                         Object attributes = jObject.get("attributes");
-                        if(attributes instanceof JSONArray) {
+                        if (attributes instanceof JSONArray) {
                             JSONArray jAtts = (JSONArray) attributes;
-                            for(Object att : jAtts) {
-                                if(att instanceof JSONObject) {
+                            for (Object att : jAtts) {
+                                if (att instanceof JSONObject) {
                                     JSONObject jAtt = (JSONObject) att;
-                                    if(propertyName.equals(jAtt.get("name"))) {
+                                    if (propertyName.equals(jAtt.get("name"))) {
                                         return true;
                                     }
                                 }
@@ -80,19 +113,28 @@ public class UMLContextUtils {
         }
         return false;
     }
-    
+
+    /**
+     * Checks if {@code associationName} is an actual association of 
+     * {@code className} in this UML context.
+     *
+     * @param context the UML context
+     * @param className the class name
+     * @param associationName the association name
+     * @return true, if is association is valid (and the class is in the context)
+     */
     public static boolean isAssociationOfClass(JSONArray context, String className, String associationName) {
-        if(context != null && className != null && associationName != null) {
-            for(Object entity : context) {
-                if(entity instanceof JSONObject) {
+        if (context != null && className != null && associationName != null) {
+            for (Object entity : context) {
+                if (entity instanceof JSONObject) {
                     JSONObject jObject = (JSONObject) entity;
-                    if(jObject.containsKey("association")) {
+                    if (jObject.containsKey("association")) {
                         Object ends = jObject.get("ends");
                         Object classes = jObject.get("classes");
-                        if(ends instanceof JSONArray && classes instanceof JSONArray) {
+                        if (ends instanceof JSONArray && classes instanceof JSONArray) {
                             JSONArray jEnds = (JSONArray) ends;
                             JSONArray jClasses = (JSONArray) classes;
-                            if(className.equals(jClasses.get(0)) && associationName.equals(jEnds.get(1))
+                            if (className.equals(jClasses.get(0)) && associationName.equals(jEnds.get(1))
                                     || className.equals(jClasses.get(1)) && associationName.equals(jEnds.get(0))) {
                                 return true;
                             }
